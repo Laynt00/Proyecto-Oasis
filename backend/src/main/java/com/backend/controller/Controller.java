914e4c753017631736d839891e6650c22663f3c3
Jose <jatb.09696@gmail.com>
2025-07-02 19:24:08 +0200

    Algo de avance springboot pero poco

---
+++ backend/src/main/java/com/backend/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.backend.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cosas")
+public class Controller {
+
+    @GetMapping
+
+    @PostMapping
+
+    @PutMapping
+
+    @DeleteMapping
+}
---
+++ backend/src/main/java/com/backend/controller/Cosas.java
@@ -0,0 +1,8 @@
+package com.backend.controller;
+
+public class Cosas {
+    private int ID;
+    private String nombre;
+    private String descripcion;
+}
+
