738291e4d03b860dd7b11d55822290ee81123197
Álvaro Torres G <laynt00@gmail.com>
2025-07-04 17:12:11 +0200

    get user location on click added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,65 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
+// Configuración de iconos
+const DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = DefaultIcon;
+
+function LocationMarker() {
+  const [position, setPosition] = useState(null);
+  const map = useMapEvents({
+    click() {
+      map.locate();
+    },
+    locationfound(e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    },
+  });
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  );
+}
 
 function App() {
-  const position = [36.720, -4.420]
+  /* const initialPosition = [36.720, -4.420]; */
+  const initialPosition = [36.720, -6.420];
 
   return (
-    <>
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <div className="map-container">
+      <MapContainer 
+        center={initialPosition} 
+        zoom={16} 
+        scrollWheelZoom={true}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
+        <Marker position={initialPosition}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
+        <LocationMarker />
       </MapContainer>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
