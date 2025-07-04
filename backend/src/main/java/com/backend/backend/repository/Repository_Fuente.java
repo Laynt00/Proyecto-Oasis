f03db82d27cb259992b39c1883bbd2ac076f3056
Jose <jatb.09696@gmail.com>
2025-07-04 18:48:46 +0200

    Crear rama Junior

---
+++ backend/src/main/java/com/backend/backend/repository/Repository_Fuente.java
@@ -0,0 +1,8 @@
+package com.backend.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.backend.backend.model.Fuente;
+
+public interface Repository_Fuente extends JpaRepository<Fuente, Long> {}
+
+
