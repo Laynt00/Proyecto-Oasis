fffe20e1218bb53bca053da7de920fd49f596014
Mario García López <mariogarlop03@gmail.com>
2025-07-05 12:53:54 +0200

    Arreglo de archivos css y estructuracion de componentes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,34 +20,34 @@ import FilterDropdown from "./components/FilterDropdown";
 function App() {
 
   // Configuración de iconos
-const DefaultIcon = L.icon({
-  iconUrl: icon,
-  shadowUrl: iconShadow,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-});
-L.Marker.prototype.options.icon = DefaultIcon;
-
-function LocationMarker() {
-  const [position, setPosition] = useState(null);
-  const map = useMapEvents({
-    click() {
-      map.locate();
-    },
-    locationfound(e) {
-      setPosition(e.latlng);
-      map.flyTo(e.latlng, map.getZoom());
-    },
+  const DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
   });
+  L.Marker.prototype.options.icon = DefaultIcon;
 
-  return position === null ? null : (
-    <Marker position={position}>
-      <Popup>You are here</Popup>
-    </Marker>
-  );
-}
+  function LocationMarker() {
+    const [position, setPosition] = useState(null);
+    const map = useMapEvents({
+      click() {
+        map.locate();
+      },
+      locationfound(e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+      },
+    });
+
+    return position === null ? null : (
+      <Marker position={position}>
+        <Popup>You are here</Popup>
+      </Marker>
+    );
+  }
 
   /* const initialPosition = [36.720, -4.420]; */
   const initialPosition = [36.72, -6.42];
@@ -66,11 +66,13 @@ function LocationMarker() {
           <SearchBar />
         </div>
       </div>
+      
       {view === "popup" && (
         <PopUpLogin onLogin={() => setView("login")} onRegister={() => setView("register")} />
       )}
       {view === "login" && <LoginPage />}
       {view === "register" && <RegisterPage />}
+
       <div className="map-container">
         <MapContainer center={initialPosition} zoom={16} scrollWheelZoom={true}>
           <TileLayer
