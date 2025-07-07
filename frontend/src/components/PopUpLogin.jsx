01d4d1a9c314e69a9ab08436694388e3bd895383
Mario García López <mariogarlop03@gmail.com>
2025-07-07 15:50:15 +0200

    mejoras en el popup

--- frontend/src/components/PopUpLogin.jsx
+++ frontend/src/components/PopUpLogin.jsx
@@ -1,21 +1,21 @@
 import userIcon from "../assets/userIcon.png"
 
-export default function PopUpLogin({ onLogin, onRegister }){
+export default function PopUpLogin(){
 
     return(
         <>
         <div className="popup-login">
-            <figure>
+            <figure className="user-appearance">
                 <img src={userIcon} className="userIcon"/>
-                <figcaption>USUARIO</figcaption>
+                <figcaption>USUARIO NO REGISTRADO</figcaption>
             </figure>
             <div>
                 <p>¿Eres un nuevo usuario?</p>
-                <button className="popup-button" onClick={onRegister}>Registrarse</button>
+                <button className="popup-button">Registrarse</button>
             </div>
             <div>
                 <p>¿Ya tienes una cuenta?</p>
-                <button className="popup-button" onClick={onLogin}>Iniciar Sesión</button>
+                <button className="popup-button">Iniciar Sesión</button>
             </div>
         </div>
         </>
