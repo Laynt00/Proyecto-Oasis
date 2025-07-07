020c4606cdff9d22adeb7fcc8762febe788069e4
Jose <jatb.09696@gmail.com>
2025-07-07 18:57:14 +0200

    Avance en CRUD parte 2

--- backend/src/main/java/com/backend/backend/model/Model_Usuario.java
+++ backend/src/main/java/com/backend/backend/model/Model_Usuario.java
@@ -10,26 +10,42 @@ public class Model_Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int ID;
+    private Long ID;
 
     private String nombre;
+    private String descripcion;
 
     //Constructor vacío
     public Model_Usuario(){}
 
     public Model_Usuario(String nombre, String descripcion){
         this.nombre = nombre;
+        this.descripcion = descripcion;
     }
 
     //GETTERS
+    public Long getID() {
+        return ID;
+    }
+
     public String getNombre(){
         return nombre;
     }
 
+    public String getDescripcion() {
+        return descripcion;
+    }
 
     //SETTERS
+    public void setId(Long ID) {
+        this.ID = ID;
+    }
+
     public void setNombre(String nombre){
         this.nombre = nombre;
     }
+
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+
 
 }
--- backend/src/main/java/com/backend/backend/service/Service_Usuario.java
+++ backend/src/main/java/com/backend/backend/service/Service_Usuario.java
@@ -1,16 +1,21 @@
 package com.backend.backend.service;
 
-import com.backend.backend.model.Model_Parque;
 import com.backend.backend.model.Model_Usuario;
 import com.backend.backend.repository.Repository_Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class Service_Usuario {
     @Autowired
-    private Repository_Usuario repositoryUsuario;
+    private final Repository_Usuario repositoryUsuario;
+
+    public Service_Usuario(Repository_Usuario repositoryUsuario) {
+        this.repositoryUsuario = repositoryUsuario;
+    }
 
     public List<Model_Usuario> findAll() {
         return repositoryUsuario.findAll();
@@ -24,7 +29,8 @@ public class Service_Usuario {
         return repositoryUsuario.save(usuario);
     }
 
-    public Model_Usuario actualizar(Long id, Model_Usuario usuario) {
+    public Model_Usuario actualizar(Long ID, Model_Usuario usuario) {
+        usuario.setId(ID);
         return repositoryUsuario.save(usuario);
     }
 
