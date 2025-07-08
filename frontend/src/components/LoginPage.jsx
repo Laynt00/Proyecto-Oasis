38955e844813b58763d2a333fb0ac0166b37257f
Mario García López <mariogarlop03@gmail.com>
2025-07-08 19:08:39 +0200

    feat: página de login creada y diseño listo

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,24 +1,36 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import emailIcon from "../assets/emailIcon.png"
+import passwordIcon from "../assets/passwordIcon.png"
+import closeButton from "../assets/closeButton.png"
+import closeButtonHover from "../assets/closeButtonHover.png"
+
 export default function LoginPage(){
+    const navigate = useNavigate();
+    const [isHover, setHover] = useState(false);
+
     return(
         <div className="login-page-div">
-            <button className="close-button">X</button>
+            <button className="close-button" onClick={() => navigate("/")} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+                <img src={isHover ? closeButtonHover : closeButton} className="close-button-image"/>
+            </button>
             <div className="div-register">
                 <p>¡Nos alegramos de volver a verte!</p>
-                <p>Ingresa tus datos para iniciar sesión. Si no tienes una cuenta, pulsa el siguiente botón para Registrarte.</p>
-                <button>Registrarme</button>
+                <p><u>Ingresa tus datos para iniciar sesión</u>. Si no tienes una cuenta, pulsa el siguiente botón para <b>Registrarte</b> .</p>
+                <button className="button-register">Registrarme</button>
             </div>
             <div className="div-login-form">
                 <p>Iniciar Sesión</p>
                 <form className="form-login">
-                    <label htmlFor="">Correo electrónico: </label>
-                    <br />
-                    <input type="email" />
-                    <br />
-                    <label htmlFor="">Contraseña: </label>
-                    <br />
-                    <input type="password" />
-                    <br />
-                    <button>Iniciar Sesión</button>
+                    <div className="input-wrapper">
+                        <img src={emailIcon} className="form-icon" />
+                        <input type="email" placeholder="Correo electrónico" />
+                    </div>
+                    <div className="input-wrapper">
+                        <img src={passwordIcon} className="form-icon" />
+                        <input type="password" placeholder="Contraseña" />
+                    </div>
+                    <button className="button-login-form">Iniciar Sesión</button>
                 </form>
             </div>
         </div>
