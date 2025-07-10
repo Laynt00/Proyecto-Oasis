9f003a7da86cd65a14125b9d342616bb604b8295
Álvaro Torres G <laynt00@gmail.com>
2025-07-10 15:34:37 +0200

    mensajito

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -158,7 +158,7 @@ function Home() {
       )}
 
       <div className="map-container">
-        <MapContainer center={initialPosition} zoom={16} scrollWheelZoom={true}>
+        <MapContainer center={initialPosition} zoom={16} scrollWheelZoom={true} zoomControl={false}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
