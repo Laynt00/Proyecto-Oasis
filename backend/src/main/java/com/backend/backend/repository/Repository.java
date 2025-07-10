be914eba229c1d60cdf228f8b89f9ca201cb9624
Jose <jatb.09696@gmail.com>
2025-07-10 18:45:48 +0200

    unesed objects deleted

--- backend/src/main/java/com/backend/backend/repository/Repository.java
+++
@@ -1,8 +0,0 @@
-package com.backend.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.backend.backend.model.Model;
-
-public interface Repository extends JpaRepository<Model, Long> {}
-
-
