e631d277c76b0529c451c004a13a9db4f45e5e01
martavicario <martacoky@gmail.com>
2025-07-14 15:43:05 +0200

    refactorizacion de archivos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
-import WelcomePage from './components/WelcomePage';
+import Map from './components/Map';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<WelcomePage />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/map" element={<Map />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
     </Routes>
