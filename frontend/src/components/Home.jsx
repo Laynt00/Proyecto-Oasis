09855db9a351fb14be28ee21d34360f141060962
Mario García López <mariogarlop03@gmail.com>
2025-07-14 19:20:55 +0200

    feat: avances en la pagina home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,55 +1,27 @@
 import { useNavigate } from "react-router-dom";
 import "./Home.css";
 import logoReact from "../assets/oasis.png";
-import fuenteImg from "../assets/fuente.png";
-import bancoImg from "../assets/banco.png";
-import perroImg from "../assets/perro.png";
 
 export default function Home() {
   const navigate = useNavigate();
 
   return (
-    <div className="welcome-container">
-      <h1>Bienvenido a Oasis</h1>
-      <h2>
-        <b>Conoce tu ciudad. Encuentra aquello que está escondido.</b>
-      </h2>
-
-      <div className="content-row">
-        <div className="gallery-div">
-          <p>
-            Busca <strong>fuentes</strong> para hidratarte, <strong>bancos</strong> para sentarte a cotillear, y{" "}
-            <strong>parques caninos</strong> donde tu perro socializa más que tú.
-          </p>
-
-          <div className="features">
-            <div className="feature-card">
-              <img src={fuenteImg} alt="Fuente" />
-              <span>Hidratación pública</span>
-            </div>
-            <div className="feature-card">
-              <img src={bancoImg} alt="Banco" />
-              <span>Descansa donde se cuece el barrio</span>
-            </div>
-            <div className="feature-card">
-              <img src={perroImg} alt="Perro" />
-              <span>Tu perro necesita socializar, tú también</span>
-            </div>
-          </div>
-        </div>
-
-        <div className="buttons-div">
-          <p>
-            <strong>Pincha en nuestro logo</strong> para empezar a usar nuestra web
-          </p>
-          <img
-            src={logoReact}
-            alt="Logo"
-            className="logo"
-            onClick={() => navigate("/map")}
-            style={{ cursor: "pointer" }}
-          />
-        </div>
+    <div className="home-page-content">
+      <div className="about-us-div">
+        <p>sobre nosotros</p>
+        <p>En Oasis, creemos que las ciudades están llenas de pequeños tesoros ocultos: una <span className="underlined-text">fuente de agua fresca</span> en verano, un <span className="underlined-text">banco a la sombra</span> para descansar, un <span className="underlined-text">parque canino</span>  donde tu mascota pueda correr libremente... Espacios útiles, accesibles y muchas veces invisibles. <br /><br />
+        Nuestra misión es ayudarte a encontrar esos lugares que no suelen estar tan señalizados, pero que marcan la diferencia en tu día a día. Oasis nace con la idea de ser ese punto de referencia inesperado, como <span className="underlined-text">un lugar de descanso en mitad del camino</span> , donde recargar energías y reconectar con tu entorno urbano. <br /><br />
+        A través de un mapa interactivo y colaborativo, te invitamos a descubrir y compartir estos rincones. Porque cada persona debería tener a su alcance un pequeño oasis, sin importar por dónde camine.</p>
+      </div>
+      <div className="logo-div">
+        <p>Pincha aquí para acceder al mapa</p>
+        <img
+          src={logoReact}
+          alt="Logo"
+          className="logo"
+          onClick={() => navigate("/map")}
+          style={{ cursor: "pointer" }}
+        />
       </div>
     </div>
   );
