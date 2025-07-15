e3bbc03568cabafba15cef3330ff32d1426707bc
Jose <jatb.09696@gmail.com>
2025-07-15 18:00:12 +0200

    Errores de comentario resueltos

--- backend/src/main/java/com/backend/backend/model/Lugar.java
+++ backend/src/main/java/com/backend/backend/model/Lugar.java
@@ -3,6 +3,9 @@ package com.backend.backend.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "tipo_lugar", discriminatorType = DiscriminatorType.STRING)
@@ -14,6 +17,11 @@ public class Lugar {
     private String nombre;
     private double x;
     private double y;
+
+    @OneToMany
+    @JoinColumn (name="id_comentario")
+    private List<Comentario> comentarios = new ArrayList<>();
+
 
     public Lugar(){}
 
@@ -36,5 +44,9 @@ public class Lugar {
 
     public double getY() { return y; }
     public void setY(double y) { this.y = y; }
+
+    public Comentario getComentario() { return comentario; }
+
+    public void setComentario(Comentario comentario) { this.comentario = comentario; }
 
 }
