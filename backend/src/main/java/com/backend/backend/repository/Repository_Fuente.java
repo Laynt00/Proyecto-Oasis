ebff3f9c91922a210a1b4ed27e181321106cc7ae
Álvaro Torres G <laynt00@gmail.com>
2025-07-15 20:01:43 +0200

    creacion deSpringBoot

--- backend/src/main/java/com/backend/backend/repository/Repository_Fuente.java
+++
@@ -1,6 +0,0 @@
-package com.backend.backend.repository;
-
-import com.backend.backend.model.Fuente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repository_Fuente extends JpaRepository<Fuente, Long> {}
